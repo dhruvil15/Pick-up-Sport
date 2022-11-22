d60489ec07f2471cc2cfcfe6f81ada92cc50a1a3
ygong <yy346225@dal.ca>
2022-11-22 19:10:54 -0400

    Create Event layout

--- app/src/main/java/com/example/pickupsports/CreateEvent.kt
+++ app/src/main/java/com/example/pickupsports/CreateEvent.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.EditText
+import android.widget.Spinner
 import androidx.navigation.fragment.findNavController
 import com.example.pickupsports.databinding.FragmentCreateEventBinding
 import com.example.pickupsports.model.Event
@@ -14,7 +17,7 @@ import com.example.pickupsports.persistence.EventsStorage
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
-class CreateEvent : Fragment() {
+class CreateEvent : Fragment(), AdapterView.OnItemSelectedListener{
 
     private var _binding: FragmentCreateEventBinding? = null
 
@@ -35,23 +38,36 @@ class CreateEvent : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.saveEventButton.setOnClickListener {
-            val title = view.findViewById<EditText>(R.id.eventAddTitle).text
-            val body = view.findViewById<EditText>(R.id.eventAddBody).text
-
-            if(title.isNotEmpty() && body.isNotEmpty() ){
-                EventsStorage.events.add(Event(title.toString(),body.toString()))
-            }
-
-
-            findNavController().navigate(R.id.action_CreateEventFragment_to_HomeFragment)
+        val spinner: Spinner = view.findViewById(R.id.create_level_play)
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter.createFromResource(
+            this.requireContext(),
+            R.array.level_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            spinner.adapter = adapter
         }
 
+        spinner.onItemSelectedListener = this
 
+
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>, view: View?, pos: Int, id: Long) {
+        // An item was selected. You can retrieve the selected item using
+        val levelOfPlay =  parent.getItemAtPosition(pos)
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>) {
+        val levelOfPlay = "Any"
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
 }
--- app/src/main/java/com/example/pickupsports/model/Event.kt
+++ app/src/main/java/com/example/pickupsports/model/Event.kt
@@ -2,7 +2,6 @@ package com.example.pickupsports.model
 
 /**
  * An event object
- * TODO: add features of sports event
  */
 class Event (var eventTitle : String, var eventBody : String) {
 
