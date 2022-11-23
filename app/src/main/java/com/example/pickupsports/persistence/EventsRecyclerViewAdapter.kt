5f835d7b9572e243264ef33a1886dc75c76932ec
yuxuanw <yx703587@dal.ca>
2022-11-23 03:46:57 -0400

    add getIcon() to Adapter, add icons to drawable folder

--- app/src/main/java/com/example/pickupsports/persistence/EventsRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/pickupsports/persistence/EventsRecyclerViewAdapter.kt
@@ -1,25 +1,31 @@
 package com.example.pickupsports.persistence
 
+import android.location.Location
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.cardview.widget.CardView
-import androidx.fragment.app.Fragment
-import androidx.navigation.Navigation
 import androidx.navigation.findNavController
-import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.example.pickupsports.R
 import com.example.pickupsports.model.Event
+import com.example.pickupsports.ui.loginAndRegister.UserData
+import com.firebase.ui.auth.data.model.User
 
 class EventsRecyclerViewAdapter() : RecyclerView.Adapter<EventsRecyclerViewAdapter.EventListItem>() {
     private var events = emptyList<Event>()
 
     inner class EventListItem(eventListItemView : View?) : RecyclerView.ViewHolder(eventListItemView!!){
-        val eventTitleText : TextView? = eventListItemView?.findViewById(R.id.eventTitle)
-        val eventBodyText : TextView? = eventListItemView?.findViewById(R.id.eventDate)
         val eventCard : CardView? = eventListItemView?.findViewById(R.id.eventCard)
+        val eventTitleText : TextView? = eventListItemView?.findViewById(R.id.eventTitle)
+        val eventIcon : ImageView? = eventListItemView?.findViewById(R.id.sportIcon)
+        val eventLocation: TextView? = eventListItemView?.findViewById(R.id.eventLocation)
+        val eventTime: TextView? = eventListItemView?.findViewById(R.id.eventTime)
+        val eventDate: TextView? = eventListItemView?.findViewById(R.id.eventDate)
+        val availability: TextView? = eventListItemView?.findViewById(R.id.numberOfPlayer)
+        val levelOfPlay: TextView? = eventListItemView?.findViewById(R.id.levelOfPlay)
         var eventPosition = 0
 
     }
@@ -33,8 +39,16 @@ class EventsRecyclerViewAdapter() : RecyclerView.Adapter<EventsRecyclerViewAdapt
     override fun onBindViewHolder(holder: EventListItem, position: Int) {
         val event : Event = events[position]
         holder.eventTitleText?.text = event.eventTitle
-        holder.eventBodyText?.text = event.eventBody
+        // set event icon accordingly
+        holder.eventIcon?.setImageResource(getIcon("cycling"))
 
+        // set time, date, location, availability and level of play
+        // TODO: uncomment here when event is implemented
+//        holder.eventTime?.text = event.eventTime
+//        holder.eventDate?.text = event.date
+//        holder.eventLocation?.text = event.location_text
+//        holder.availability?.text = event.participants.size + "/" + event.capacity
+//        holder.levelOfPlay?.text = event.levelOfPlay
         // click to modify a selected event
         holder.eventCard?.setOnClickListener {
             it.findNavController().navigate(R.id.action_HomeFragment_to_CreateEventFragment)
@@ -48,6 +62,22 @@ class EventsRecyclerViewAdapter() : RecyclerView.Adapter<EventsRecyclerViewAdapt
     fun setEvents(events : List<Event>){
         this.events = events;
         notifyDataSetChanged()
+    }
+
+    /**
+     * Take in [eventName] and get the corresponding icon name
+     * for demo purpose, only 5 events are selectable (and plus one default icon)
+     * @return icon
+     */
+    fun getIcon(eventName: String): Int {
+        return when (eventName) {
+            "tennis" -> R.drawable.tennis_icon
+            "football" -> R.drawable.football_icon
+            "basketball" -> R.drawable.basketball_icon
+            "cycling" -> R.drawable.cycling_icon
+            "baseball" -> R.drawable.baseball_icon
+            else -> R.drawable.default_icon
+        }
     }
 
 
