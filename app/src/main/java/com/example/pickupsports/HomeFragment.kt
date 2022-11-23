6c99385cd16d474b8ed413228cf191d348786e4e
yuxuanw <yx703587@dal.ca>
2022-11-22 23:59:30 -0400

    add click ripple effect on card view

--- app/src/main/java/com/example/pickupsports/HomeFragment.kt
+++ app/src/main/java/com/example/pickupsports/HomeFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.cardview.widget.CardView
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -36,6 +37,7 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        // float button: shortcut to create a event
         binding.addEventButton.setOnClickListener {
             findNavController().navigate(R.id.action_HomeFragment_to_CreateEventFragment)
         }
@@ -54,4 +56,6 @@ class HomeFragment : Fragment() {
         super.onDestroyView()
         _binding = null
     }
+
+
 }
--- app/src/main/java/com/example/pickupsports/persistence/EventsRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/pickupsports/persistence/EventsRecyclerViewAdapter.kt
@@ -4,17 +4,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.example.pickupsports.R
 import com.example.pickupsports.model.Event
 
-class EventsRecyclerViewAdapter() : RecyclerView.Adapter<EventsRecyclerViewAdapter.EventListItem>(){
+class EventsRecyclerViewAdapter() : RecyclerView.Adapter<EventsRecyclerViewAdapter.EventListItem>() {
     private var events = emptyList<Event>()
 
     inner class EventListItem(eventListItemView : View?) : RecyclerView.ViewHolder(eventListItemView!!){
         val eventTitleText : TextView? = eventListItemView?.findViewById(R.id.eventTitle)
         val eventBodyText : TextView? = eventListItemView?.findViewById(R.id.eventDate)
+        val eventCard : CardView? = eventListItemView?.findViewById(R.id.eventCard)
         var eventPosition = 0
+
+        // click on event card view: modify event
+//        eventCard.setOnClickListener {
+//            println("test")
+//            // findNavController().navigate(R.id.action_HomeFragment_to_CreateEventFragment)
+//        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EventListItem {
@@ -27,6 +38,9 @@ class EventsRecyclerViewAdapter() : RecyclerView.Adapter<EventsRecyclerViewAdapt
         val event : Event = events[position]
         holder.eventTitleText?.text = event.eventTitle
         holder.eventBodyText?.text = event.eventBody
+        holder.itemView.setOnClickListener {
+            println("test")
+        }
     }
 
     override fun getItemCount(): Int {
@@ -37,4 +51,6 @@ class EventsRecyclerViewAdapter() : RecyclerView.Adapter<EventsRecyclerViewAdapt
         this.events = events;
         notifyDataSetChanged()
     }
+
+
 }
